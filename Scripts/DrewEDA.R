2b35f2f2903ced9f1c76188e35a3da7b4baf9f4b
drew-daw <163182379+drew-daw@users.noreply.github.com>
2025-11-16 13:32:43 +0000

    shits and gigs innit

--- Scripts/DrewEDA.R
+++ Scripts/DrewEDA.R
@@ -3,5 +3,8 @@ library(tidyverse)
 load("cleanedData.RData")
 
 #Question 1
-
+data %>% ggplot(aes(Value, ))
 #Question 3
+
+nrow(data[!is.na(data$`Education level`),])
+unique(data$`Education level`)
